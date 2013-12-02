aaa79a23133f1e0057409cff1f16a0da303e3498
gjp465 <guillermo.jimenez@imdea.org>
2013-12-02 11:21:53 +0100

    - New REST structure - New Features implemented -+get api info -+update windowsize -PEP8 cleaning -Running from obtained

--- cloto/manager/InfoManager.py
+++ cloto/manager/InfoManager.py
@@ -4,18 +4,23 @@ from cloto.models import TenantInfo, ServerInfo
 
 
 class InfoManager():
+    """This class provides methods to manage information about the server and windowsize of tenants.
+    """
 
     def __init__(self):
         self.tenantInfo = self.get_tenant_information()
         self.serverInfo = self.get_server_information()
 
     def get_server_information(self):
+        """Returns model of Information about server."""
         return ServerInfo
 
     def get_tenant_information(self):
+        """Returns model of Information about a tenant."""
         return TenantInfo
 
     def get_information(self, tenantId):
+        """Returns information about the server and tenant's windowsize."""
         serverInfo = self.get_server_information()
         tenantInfo = self.get_tenant_information()
         s_query = serverInfo.objects.get(id__exact='1')
@@ -28,10 +33,12 @@ class InfoManager():
         return information.information(owner, windowsize, version, runningfrom, doc)
 
     def updateWindowSize(self, tenantId, newSize):
+        """Updates windowsize of a specified tenant."""
         t = self.tenantInfo.objects.get(tenantId__exact=tenantId)
         t.windowsize = newSize
         t.save()
 
     def setInformations(self, sInfo, tInfo):
+        """Sets server information and tenant information to the InfoManager."""
         self.tenantInfo = tInfo
         self.serverInfo = sInfo

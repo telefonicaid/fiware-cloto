030b39249a96a9728239bff1d0e414ef3451caaa
Toni Robres <arobres@tid.es>
2014-03-21 15:20:38 +0100

    introduce errors in mock

--- cloto/tests/acceptance_tests/commons/server_mock.py
+++ cloto/tests/acceptance_tests/commons/server_mock.py
@@ -2,7 +2,7 @@ __author__ = 'arobres'
 
 
 from bottle import route, run, template, Bottle, request, response, auth_basic
-from collections import defaultdict
+from collections import deque
 import ujson
 
 app = Bottle()
@@ -10,10 +10,38 @@ statistics = {'num_scale_up': 0,
               'num_scale_down': 0,
               'num_notifications': 0}
 
+responses_error = deque()
+
+@app.post('/save_response/')
+def save_response():
+
+    body = "".join(request.body)
+    try:
+        body = ujson.loads(body)
+    except:
+        response.status = 400
+        return {"message": "The JSON format is not correct"}
+
+    if 'error_code' not in body.keys():
+        response.status = 400
+        return {"message": "Error code is not included in the request"}
+    else:
+        responses_error.append(body['error_code'])
+
+
+@app.get('/reset_errors/')
+def reset_errors():
+
+    responses_error.clear()
+
 
 @app.post("/scale_up/")
 def scale_up():
-    statistics['num_scale_up'] += 1
+    if responses_error:
+        response.status = int(responses_error.popleft())
+        return {"message": "Error response: {}".format(response.status)}
+    else:
+        statistics['num_scale_up'] += 1
 
 
 @app.post("/scale_down/")
@@ -39,6 +67,3 @@ def get_stats():
     return statistics
 
 run(app, host='0.0.0.0', port=8080, reloader=True)
-
-
-

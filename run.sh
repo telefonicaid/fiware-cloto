63ea391f2647341c751b9bcbf08002aae61d2e9c
gjp465 <guillermo.jimenez@imdea.org>
2014-04-29 12:51:45 +0200

    Getting IP for all Unix OS.

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 # __author__ = 'gjp'
 
 OS=`uname`
-IP=`ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}'`;
+IP=`ping -c 1 $HOSTNAME | grep 'PING' | awk '{print $3}' | sed 's/[:()]//g'`;
 
 cd /opt/policyManager/fiware-cloto
 python manage.py runserver $IP:8000

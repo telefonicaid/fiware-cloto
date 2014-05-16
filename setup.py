0ac99151e3018e6fc1a618486c8464b33be08e86
geonexus <e.fiware@tid.es>
2014-05-16 16:05:24 +0200

    adding setup.py to upload cloto as pypi package

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+#
+# Copyright 2014 Telefónica Investigación y Desarrollo, S.A.U
+#
+# This file is part of FI-WARE project.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+#
+# You may obtain a copy of the License at:
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# For those usages not covered by the Apache version 2.0 License please
+# contact with opensource@tid.es
+#
+from distutils.core import setup
+setup(
+  name = 'cloto',
+  packages = ['cloto'], # this must be the same as the name above
+  version = '1.0',
+  description = 'This module is part of FI-WARE Policy Manager. It provides an API-REST for create rules associated '
+                'to servers, subscribe servers to Context Broker to get information about resources consumption of that'
+                ' servers and launch actions described in rules when conditions are given.',
+  author = 'Fernando Lopez Aguilar, Guillermo Jimenez Prieto',
+  author_email = 'fla@tid.es, e.fiware@tid.es',
+  url = 'https://github.com/telefonicaid/fiware-cloto', # use the URL to the github repo
+  download_url = 'https://github.com/telefonicaid/fiware-cloto/tarball/1.0', # I'll explain this in a second
+  keywords = ['fiware', 'policy', 'manager', 'cloud'], # arbitrary keywords
+  classifiers = [],
+)

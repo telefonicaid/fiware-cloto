7643d24d9b916da79625c2a63eecfded499456c8
Guillermo Jimenez Prieto <e.fiware.tid@telefonica.com>
2015-02-09 13:23:52 +0100

    fix: dev: fix clips dependencies @minor

--- build.sh
+++ build.sh
@@ -31,11 +31,22 @@ virtualenv ENV
 source ENV/bin/activate
 mkdir -m 777 /var/log/fiware-cloto
 pip install -r requirements.txt
+
+#PYCLIPS installation
+wget -O pyclips.tar.gz http://downloads.sourceforge.net/project/pyclips/pyclips/pyclips-1.0/pyclips-1.0.7.348.tar.gz?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fpyclips%2Ffiles%2Fpyclips%2Fpyclips-1.0%2F&ts=1423484225&use_mirror=softlayer-ams
+tar -xvzf pyclips.tar.gz
+cd pyclips
+python setup.py build
+su -c "python setup.py install"
+cd ..
+
 export DJANGO_SETTINGS_MODULE=settings.settings_tests
 export SETTINGS_TYPE=test
 echo "no" | python manage.py syncdb
 coverage run --source=cloto,orion_wrapper,environments manage.py test
 coverage xml -o target/site/cobertura/coverage.xml
+
+
 
 if [ ! $1 = "travis_build" ];
 then

30b04ef313e68e7d4593fd0698364767d090e29e
visitor <henar.munozfrutos@telefonica.com>
2016-05-25 16:33:55 +0200

    Update user domain name

--- docker/start.sh
+++ docker/start.sh
@@ -3,6 +3,8 @@ sed -i -e "s/{ADM_PASSWORD}/${ADM_PASSWORD}/" /etc/fiware.d/fiware-cloto.cfg
 sed -i -e "s/{KEYSTONE_IP}/${KEYSTONE_IP}/" /etc/fiware.d/fiware-cloto.cfg
 sed -i -e "s/{ADM_TENANT_ID}/${ADM_TENANT_ID}/" /etc/fiware.d/fiware-cloto.cfg
 sed -i -e "s/{ADM_USERNAME}/${ADM_USERNAME}/" /etc/fiware.d/fiware-cloto.cfg
+sed -i -e "s/{OS_USER_DOMAIN_NAME}/${OS_USER_DOMAIN_NAME}/" /etc/fiware.d/fiware-cloto.cfg
+
 while ! nc -z mysql 3306; do sleep 8; done
 python setup.py sdist
 pip install dist/fiware-cloto-2.6.0.tar.gz
